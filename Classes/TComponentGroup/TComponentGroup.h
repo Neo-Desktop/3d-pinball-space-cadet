531e6381793c2f752a322098ad29aa9e6b9cbaf1
Sydney Biscuit <Neo-Desktop@users.noreply.github.com>
2019-08-18 15:27:35 -0700

    Remove function decorators from TComponentGroup

--- Classes/TComponentGroup/TComponentGroup.h
+++ Classes/TComponentGroup/TComponentGroup.h
@@ -10,11 +10,11 @@
 /* 127 */
 struct TComponentGroup;
 
-void __stdcall TComponentGroup::NotifyTimerExpired(int, struct TPinballComponent*); // idb
-int __thiscall TComponentGroup::Message(TComponentGroup* this, int, float); // idb
-TComponentGroup* __thiscall TComponentGroup::TComponentGroup(TComponentGroup* this, struct TPinballTable* a2, int a3);
-TZmapList* __thiscall TComponentGroup::~TComponentGroup(TComponentGroup* this);
-TComponentGroup* __thiscall TComponentGroup::`scalar deleting destructor`(TComponentGroup *this, char a2);
+void TComponentGroup::NotifyTimerExpired(int, struct TPinballComponent*); // idb
+int TComponentGroup::Message(TComponentGroup* this, int, float); // idb
+TComponentGroup* TComponentGroup::TComponentGroup(TComponentGroup* this, struct TPinballTable* a2, int a3);
+TZmapList* TComponentGroup::~TComponentGroup(TComponentGroup* this);
+TComponentGroup* TComponentGroup::destroy(TComponentGroup *this, char a2);
 
 void* TComponentGroup::vftable = &TComponentGroup::Message; // weak
 
